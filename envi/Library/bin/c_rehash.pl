7805b4b91c34b4f893e59e9251ae179e67cfc51f
abhinavdwivedilko16 <abhinavdwivedilko16@gmail.com>
2023-03-24 15:55:02 +0530

    EDA and Problem Statement

--- envi/Library/bin/c_rehash.pl
+++ envi/Library/bin/c_rehash.pl
@@ -13,7 +13,7 @@
 # and add symbolic links to their hash values.
 
 my $dir = "C:\\Program Files\\Common Files\\ssl";
-my $prefix = "D:/first_ml_project/envi\\Library";
+my $prefix = "d:/first_ml_project/envi\\Library";
 
 my $errorcount = 0;
 my $openssl = $ENV{OPENSSL} || "openssl";
