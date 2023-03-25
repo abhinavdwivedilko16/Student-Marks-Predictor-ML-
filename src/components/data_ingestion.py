dd6b63350398ae4b5fd56f55e70e8f970c43594f
abhinavdwivedilko16 <abhinavdwivedilko16@gmail.com>
2023-03-25 21:28:05 +0530

    data transformation done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -3,47 +3,53 @@ import sys
 from src.exception import CustomException
 from src.logger import logging
 import pandas as pd
+
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-@dataclass # we will be able to directly define the class variable
-class DataIngestionConfig:
-    train_data_path: str=os.path.join("artifacts","train.csv")  # in the artifact folder we will see the output
-    test_data_path: str=os.path.join("artifacts","test.csv")  # in the artifact folder we will see the output
-    raw_data_path: str=os.path.join("artifacts","data.csv")  # in the artifact folder we will see the output
-    # above mentioned are the input for the data Ingestion component and now data Ingestion component knows where to save the output
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
 
-# If we have to only initilize the variables then go with the dataclass, 
-# And if there are functions inside class then go with __init__ part.
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str=os.path.join('artifacts',"train.csv")
+    test_data_path: str=os.path.join('artifacts',"test.csv")
+    raw_data_path: str=os.path.join('artifacts',"data.csv")
 
 class DataIngestion:
     def __init__(self):
-        self.ingestion_config=DataIngestionConfig()  #as soon as we call this class, the above 3 mentioned path will save inside this class variable
+        self.ingestion_config=DataIngestionConfig()
 
-    def initiate_data_ingestion(self): # if our data is stored in some database then we will write code here to read that data.
-        logging.info("Entered in data ingestion component")
+    def initiate_data_ingestion(self):
+        logging.info("Entered the data ingestion method or component")
         try:
             df=pd.read_csv('notebook\data\stud.csv')
-            logging.info('Read the data set as dataframe')
+            logging.info('Read the dataset as dataframe')
 
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)  #creating folder with respect to the paths we defined in dataingestion component
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
+
             df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+
             logging.info("Train test split initiated")
             train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
 
             train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+
             test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
-            logging.info("Ingestion of the data completed")
+
+            logging.info("Inmgestion of the data iss completed")
 
             return(
-
                 self.ingestion_config.train_data_path,
                 self.ingestion_config.test_data_path
-            )
 
+            )
         except Exception as e:
             raise CustomException(e,sys)
-            
+        
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
---
+++ src/utils.py
@@ -0,0 +1,21 @@
+
+# this peice of code is written to save the pkl file in hard disk
+import os
+import sys
+import pandas as pd
+import numpy as np
+import dill 
+from src.exception import CustomException
+from src.logger import logging 
+
+def save_object(file_path,obj):
+    try:
+        dir_path= os.path.dirname(file_path)
+
+        os.makedirs(dir_path,exist_ok=True)
+
+        with open(file_path,"wb") as file_obj:
+            dill.dump(obj,file_obj)          #dill is used to create a pkl file 
+
+    except Exception as e:
+        raise CustomException(e,sys)
