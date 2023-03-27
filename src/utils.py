33b95b2eae5e6b13daee51862c0130f25f5b8c38
abhinavdwivedilko16 <abhinavdwivedilko16@gmail.com>
2023-03-27 11:02:44 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,3 @@
-
-# this peice of code is written to save the pkl file in hard disk
 import os
 import sys
 import pandas as pd
@@ -7,6 +5,7 @@ import numpy as np
 import dill 
 from src.exception import CustomException
 from src.logger import logging 
+from sklearn.metrics import r2_score
 
 def save_object(file_path,obj):
     try:
@@ -17,5 +16,31 @@ def save_object(file_path,obj):
         with open(file_path,"wb") as file_obj:
             dill.dump(obj,file_obj)          #dill is used to create a pkl file 
 
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+def evaluate_models(X_train,y_train,X_test,y_test,models):
+    try:
+        report= {}
+
+        for i in range(len(list(models))):
+
+
+            model = list(models.values())[i]
+
+            model.fit(X_train,y_train) #Train Model
+
+            y_train_pred= model.predict(X_train)
+
+            y_test_pred=model.predict(X_test)
+
+            train_model_score= r2_score(y_train, y_train_pred)
+
+            test_model_score= r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+    
     except Exception as e:
         raise CustomException(e,sys)
